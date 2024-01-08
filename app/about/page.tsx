f0e3a6d212e75cd3522aaea66823a91f5348c4ca
Aflu <mohamedaflah186@gmail.com>
2024-01-08 18:35:13 +0530

    initial commit

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+function About(){
+    return(
+        <div>
+            <h1 className="text-2xl text-blue-400 text-center">About</h1>
+            <h2>Sir I know this confidential </h2>
+        </div>
+    )
+}
+export default About
